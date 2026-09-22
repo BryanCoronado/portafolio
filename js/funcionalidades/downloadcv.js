1c1a081d01c57fbb3e3579bb511b362f992490af
CoronadoBryan <smithelunico1234@gmail.com>
2026-09-22 12:08:58 -0500

    first commit

---
+++ js/funcionalidades/downloadcv.js
@@ -0,0 +1,23 @@
+let ruta = "https://bryancito.space/documents/cv.pdf"
+let nombre = "cv-bryan-coronado-santi.pdf"
+
+
+function Descargar_boton (ruta,nombre){
+    const enlace = document.createElement("a")
+    enlace.href=ruta
+    enlace.download = nombre
+    enlace.click()
+}
+
+
+const boton_descargar = document.getElementById("download-cv")
+boton_descargar.addEventListener("click",function(){
+    Descargar_boton(ruta,nombre);
+})
+
+const boton_descargar2 = document.getElementById("download-cv2")
+boton_descargar2.addEventListener("click",function(){
+    Descargar_boton(ruta,nombre);
+})
+
+
